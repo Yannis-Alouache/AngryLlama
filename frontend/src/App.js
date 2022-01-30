23470936df0f97d47e32947d458b273ef7aaaabe
Yannis-Alouache <alouacehyannis1@gmail.com>
2022-01-30 15:59:31 +0100

    [+]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import {
   Container,
   Row,
   Col,
-  Image
+  Image,
+  Card,
+  Button
 } from "react-bootstrap";
 
 import MyNavbar from './Components/Navbar'
@@ -16,6 +18,8 @@ import e from './Assets/Images/gallery/e.png';
 import f from './Assets/Images/gallery/f.png';
 import g from './Assets/Images/gallery/g.png';
 import h from './Assets/Images/gallery/h.png';
+import avatar from './Assets/Images/avatar.png'
+
 
 function App() {
   return (
@@ -97,12 +101,35 @@ function App() {
         </Container>
       </section>
 
-      <section>
-        <Container>
+      <section className="pt-5">
+      <Container>
+        <Card className="text-center p-3 team-card-bg">
+        <Card.Title>Meet the team</Card.Title>
+          <Card.Img src={avatar} className="w-10 rounded-max mx-auto"/>
+          <Card.Body>
+            <Card.Text className="">
+              Yannis is a junior developper, that is passionate with the blockchain technology and the web in general.<br></br>
+              He also love Llama !
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
+        {/* <Container>
           <h1 className="text-center pt-5">The Team</h1>
           <p className="text-center">Take a look at the team of the Llama</p>
-          <br></br>
-        </Container>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={avatar} />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the bulk of
+                the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Container> */}
       </section>
 
     </div>
